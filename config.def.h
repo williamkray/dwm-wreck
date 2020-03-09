e752349bada7c95bc53d30905b513335bc4d5274
William Kray <william@kray.pw>
2020-03-09 13:57:55 -0700

    move personal config.h into config.def.h to make updates easier later

--- config.def.h
+++ config.def.h
@@ -1,25 +1,32 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Deja Vu Sans Mono:style=book:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+static const char col_cyan[]        = "#5E8D87";
+static const char col_black[]       = "#282A2E";
+static const char col_bblack[]      = "#373B41";
+static const char col_white[]       = "#707880";
+static const char col_bwhite[]      = "#C5C8C6";
+static const char col_bcyan[]       = "#8ABEB7";
+static const char col_green[]       = "#8C9440";
+static const int  gappx             = 20;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_bwhite, col_bblack, col_black },
+	[SchemeSel]  = { col_green, col_black, col_green  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -27,12 +34,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ NULL,       NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.65; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
@@ -44,7 +50,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,13 +62,37 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "rofi", "-modi", "drun,run", "-show", "drun", "-monitor", "primary", "-show-icons", "-sidebar-mode", NULL };
+static const char *termcmd[]  = { "st", "-e", "tmux.sh", NULL };
+static const char *lockcmd[] = { "screen-locker.sh", NULL };
+static const char *browsecmd[] = { "firefox", NULL };
+static const char *prntselcmd[] = { "screencap.sh", "some", NULL };
+static const char *prntallcmd[] = { "screencap.sh", "all", NULL };
+static const char *volupcmd[] = { "pulsectl.sh", "up", NULL };
+static const char *voldncmd[] = { "pulsectl.sh", "down", NULL };
+static const char *voltglcmd[] = { "pulsectl.sh", "toggle", NULL };
+static const char *brtnessupcmd[] = { "light", "-A", "5", NULL };
+static const char *brtnessdncmd[] = { "light", "-U", "5", NULL };
+static const char *micmutecmd[] = { "pulse-mic-mute.sh", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+  { MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
+  { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = browsecmd } },
+  { 0,                         XK_Print,  spawn,          {.v = prntallcmd } },
+  { ShiftMask,                    XK_Print,  spawn,          {.v = prntselcmd } },
+  { 0,                         0x1008ff13, spawn,         {.v = volupcmd } },
+  { 0,                         0x1008ff11, spawn,         {.v = voldncmd } },
+  { 0,                         0x1008ff12,  spawn,        {.v = voltglcmd } },
+  { 0,                         0x1008ff03,  spawn,        {.v = brtnessdncmd } },
+  { 0,                         0x1008ff02,  spawn,        {.v = brtnessupcmd } },
+  { 0,                         0x1008ffb2,  spawn,        {.v = micmutecmd } },
+  { MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+  { MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+  { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -88,11 +118,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
