90ebd384e6d78849546e61d2d064e09d8ce4fc19
William Kray <william@kray.pw>
2020-03-09 10:22:06 -0700

    add systray patch

--- config.h
+++ config.h
@@ -7,6 +7,10 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Deja Vu Sans Mono:style=book:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const char col_cyan[]        = "#5E8D87";
 static const char col_black[]       = "#282A2E";
 static const char col_bblack[]      = "#373B41";
