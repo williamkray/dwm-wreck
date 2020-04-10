0f6223b6c04b6e7e42d093d3cd44f816b346499f
William Kray <github@williamkray.com>
2020-04-10 10:41:28 -0700

    include symbola font and mic mute button

--- config.def.h
+++ config.def.h
@@ -5,7 +5,10 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Deja Vu Sans Mono:style=book:size=10" };
+static const char *fonts[]          = { 
+  "Deja Vu Sans Mono:style=book:size=10",
+  "Symbola:pixelsize=10"
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -90,6 +93,7 @@ static Key keys[] = {
   { 0,                            0x1008ff03, spawn,         {.v = brtnessdncmd } },
   { 0,                            0x1008ff02, spawn,         {.v = brtnessupcmd } },
   { 0,                            0x1008ffb2, spawn,         {.v = micmutecmd } },
+  { 0,                            XK_F4,     spawn,          {.v = micmutecmd } },
   { MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
   { MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
   { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
